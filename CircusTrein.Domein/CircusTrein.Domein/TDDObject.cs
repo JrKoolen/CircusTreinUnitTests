df52a9986319b0bbb2287788c1dffd1b34dec352
JrKoolen <81234655+JrKoolen@users.noreply.github.com>
2024-06-17 15:07:27 +0200

    update

---
+++ CircusTrein.Domein/CircusTrein.Domein/TDDObject.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircusTrein.Domein
+{
+    public class TDDObject
+    {
+        public int TotalAmount { get; set; }
+
+        public TDDObject(int totalAmount) 
+        {
+            TotalAmount = totalAmount;
+        }
+
+        public Boolean IsItAboveTotalAmount(int i)
+        {
+            TotalAmount += i;
+            if (i < TotalAmount)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public int ResetAmount() 
+        {
+            TotalAmount = 0;
+            return TotalAmount; 
+        }
+
+        public Boolean IsItAboveForAll(List<TDDObject> list, int amount)
+        {
+            bool result;
+            foreach (TDDObject obj in list)
+            {
+                result = obj.IsItAboveTotalAmount(amount);
+                if (result)
+                {
+                    return false ;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ CircusTrein.Domein/CircusTrein.UnitTests/TDDTests.cs
@@ -0,0 +1,59 @@
+﻿using CircusTrein.Domein;
+
+namespace CircusTrein.UnitTests
+{
+    public class TDDTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void Add_PositiveNumberToObject_ReturnFalse()
+        {
+            //arrange
+            int amount = 10;
+
+            //act
+            TDDObject tdd = new TDDObject(11);
+
+            //assert
+            Assert.IsFalse(tdd.IsItAboveTotalAmount(11));
+        }
+
+        [Test]
+        public void CallResetFunction_ReturnZero()
+        {
+            int amount = 10;
+
+            TDDObject tdd = new TDDObject(amount);
+
+            int result = tdd.ResetAmount();
+
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void Send_ListTDDObjects_ReturnsTrue()
+        {
+            //Arrange
+            int amount = 10;
+
+            TDDObject Original = new TDDObject(amount);
+            List<TDDObject> tdd = new List<TDDObject>()
+            {
+             new TDDObject(9),
+             new TDDObject(5),
+             new TDDObject(11),
+            };
+
+            //Act
+            Boolean result = Original.IsItAboveForAll(tdd, 12);
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+    }
+}
